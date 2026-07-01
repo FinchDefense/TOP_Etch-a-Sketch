c2a8be9edaa3df4316d6b358d1d407bc8ffb61a5
Jerry Yang <87034558+FinchDefense@users.noreply.github.com>
2026-06-30 20:08:39 -0400

    Add for loop that loops 256 times, appending createSquare(i) each time.

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 const createSquare = (index) => {
-    const square = document.createElement("div");
+    let square = document.createElement("div");
     square.classList.add('square');
     square.textContent = index + 1;
 
     return square;
+}
+
+for (let i=0; i<256; i++) {
+    document.body.appendChild(createSquare(i));
 }
