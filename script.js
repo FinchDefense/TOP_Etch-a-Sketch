a5e8db9385a9627690c90f3b67b10fea7e8adf68
Jerry Yang <87034558+FinchDefense@users.noreply.github.com>
2026-06-30 21:27:42 -0400

    Create a function that will prompt the number of squares per side for a new grid. Also an event listener which will run when the new grid button is clicked.

--- script.js
+++ script.js
@@ -4,6 +4,20 @@ const createSquare = () => {
     return square;
 }
 
+function promptMe() {
+    return prompt("Number of squares per side(max 100): ");
+}
+
+const button = document.createElement("button");
+button.textContent = "NEW GRID";
+button.addEventListener('click', () => {
+    let sps = null;
+
+    }
+)
+
+document.body.appendChild(button);
+
 const fragment = document.createDocumentFragment();
 
 for (let i = 0; i < 256; i++) {
