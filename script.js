47120af4d98284a9b37198f08488ad0da152dbc7
Jerry Yang <87034558+FinchDefense@users.noreply.github.com>
2026-07-01 09:46:15 -0400

    Re-order positions of certain pieces of code.

--- script.js
+++ script.js
@@ -1,32 +1,52 @@
+const container = document.createElement("div");
+container.classList.add('fragment');
+document.body.appendChild(container);
+
+const button = document.createElement("button");
+button.textContent = "NEW GRID";
+
+
 const createSquare = () => {
     const square = document.createElement("div");
     square.classList.add('square');
     return square;
 }
 
+newGrid(256);
+
 function promptMe() {
     return prompt("Number of squares per side(max 100): ");
 }
 
-const button = document.createElement("button");
-button.textContent = "NEW GRID";
-button.addEventListener('click', () => {
-    let sps = null;
 
+button.addEventListener('click', () => {
+    let sps = -1;
+    while (isNaN(sps) || +sps > 100 || +sps < 0) {
+        sps = promptMe();
+        if (sps === null) return;
+    }
+
+    newGrid(+sps);
     }
 )
 
 document.body.appendChild(button);
 
-const fragment = document.createDocumentFragment();
+function newGrid(sps = 256) {
+    const fragment = document.createDocumentFragment();
+    container.replaceChildren();
 
-for (let i = 0; i < 256; i++) {
-    const newSquare = createSquare();
-    fragment.appendChild(newSquare);
+    for (let i = 0; i < sps; i++) {
+        const newSquare = createSquare();
+        fragment.appendChild(newSquare);
+    }
+
+    const elements = document.querySelectorAll;
+    elements.forEach(element => {
+        element.style.width = `$(100 / +sps)vw`;
+        element.style.aspectRatio = '1 / 1';
+    })
+
+
+    container.appendChild(fragment);
 }
-
-const container = document.createElement("div");
-container.classList.add('fragment');
-container.appendChild(fragment);  
-
-document.body.appendChild(container);
