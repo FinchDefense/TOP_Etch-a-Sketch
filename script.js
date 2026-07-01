58433e624af5b8274556ddcc78f15c9950b923a6
Jerry Yang <87034558+FinchDefense@users.noreply.github.com>
2026-06-30 20:35:27 -0400

    Add an outer div by the name of 'container' with class 'fragment'. Then append container to document.body instead of fragment.

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
 const createSquare = () => {
     const square = document.createElement("div");
     square.classList.add('square');
-
     return square;
 }
 
 const fragment = document.createDocumentFragment();
 
-for (let i=0; i<256; i++) {
+for (let i = 0; i < 256; i++) {
     const newSquare = createSquare();
     fragment.appendChild(newSquare);
 }
 
-document.body.appendChild(fragment);
+const container = document.createElement("div");
+container.classList.add('fragment');
+container.appendChild(fragment);  
+
+document.body.appendChild(container);
