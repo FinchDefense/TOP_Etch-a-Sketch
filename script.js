bf38f8cd5798c2a4a3ea61d30f964347774f5c6f
Jerry Yang <87034558+FinchDefense@users.noreply.github.com>
2026-06-30 20:17:30 -0400

    Remove the index parameter from the createSquare function. Modify body of for loop to just call createSquare.

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-const createSquare = (index) => {
+const createSquare = () => {
     let square = document.createElement("div");
     square.classList.add('square');
-    square.textContent = index + 1;
+    document.body.appendChild(square);
 
     return square;
 }
 
 for (let i=0; i<256; i++) {
-    document.body.appendChild(createSquare(i));
+    createSquare();
 }
