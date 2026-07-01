71a78f6716ee28c663286bc821b1eb9671a7a596
Jerry Yang <87034558+FinchDefense@users.noreply.github.com>
2026-07-01 10:14:15 -0400

    Use inline css to finalize newGrid func. Re-order certain parts of code.

--- script.js
+++ script.js
@@ -4,8 +4,7 @@ document.body.appendChild(container);
 
 const button = document.createElement("button");
 button.textContent = "NEW GRID";
-document.body.insertBefore(button, container);
-
+document.body.insertBefore(button, container); // Insert button before container
 
 const createSquare = () => {
     const square = document.createElement("div");
@@ -13,32 +12,35 @@ const createSquare = () => {
     return square;
 }
 
+function newGrid(sps = 16) {
+    container.replaceChildren(); // clear out the container
+
+    const square_size = 100 / sps; // Each square takes up 1 / sps of the screen width
+
+    for (let i=0; i < sps * sps; i++) {
+        const square = createSquare();
+        square.style.width = `${square_size}vw`;
+        square.style.aspectRatio = "1 / 1";
+        container.appendChild(square);
+    }
+}
+
 function promptMe() {
     return prompt("Number of squares per side(max 100): ");
 }
 
+newGrid(16); // Have a grid ready as soon as web page loads
 
 button.addEventListener('click', () => {
-    let sps = -1;
+    let sps = -1; // So that it will run the first iteration 
     while (isNaN(sps) || +sps > 100 || +sps < 0) {
         sps = promptMe();
         if (sps === null) return;
     }
 
-    newGrid(+sps);
+    newGrid(+sps); // Because sps is a string, we have to convert to a number
     }
 )
 
 
 
-function newGrid(sps = 256) {
-    const fragment = document.createDocumentFragment();
-    container.replaceChildren();
-
-    for (let i = 0; i < sps; i++) {
-        const newSquare = createSquare();
-        fragment.appendChild(newSquare);
-    }
-
-    container.appendChild(fragment);
-}
